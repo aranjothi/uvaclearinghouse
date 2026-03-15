67884979010773c5b3305104887c74ee0c3b6016
Aran Jothi <aran.jothi@gmail.com>
2026-03-14 22:44:35 -0400

    quick change to settings.py

--- django/clearinghouse/settings.py
+++ django/clearinghouse/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 
-load_dotenv(Path(__file__).resolve().parent.parent.parent / '.env')
+try:
+    from dotenv import load_dotenv
+    load_dotenv(Path(__file__).resolve().parent.parent.parent / '.env')
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
